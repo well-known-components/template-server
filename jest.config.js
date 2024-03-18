3d062de56a6288ea03dc09e684be184d6519cbdb
Hugo Arregui <hugo@decentraland.org>
2024-03-18 09:48:29 -0300

    updates
    - bump dependencies
    - use yarn
    - update jest config
    - update prettier config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  globals: {
-    "ts-jest": {
-      tsconfig: "test/tsconfig.json",
-    },
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", {tsconfig: "test/tsconfig.json"}]
   },
   moduleFileExtensions: ["ts", "js"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
   coverageDirectory: "coverage",
   collectCoverageFrom: ["src/**/*.ts", "src/**/*.js"],
   testMatch: ["**/*.spec.(ts)"],
--- src/components.ts
+++ src/components.ts
@@ -1,28 +1,29 @@
-import { createDotEnvConfigComponent } from "@well-known-components/env-config-provider"
-import { createServerComponent, createStatusCheckComponent } from "@well-known-components/http-server"
-import { createLogComponent } from "@well-known-components/logger"
-import { createFetchComponent } from "./adapters/fetch"
-import { createMetricsComponent, instrumentHttpServerWithMetrics } from "@well-known-components/metrics"
-import { AppComponents, GlobalContext } from "./types"
-import { metricDeclarations } from "./metrics"
+import { createDotEnvConfigComponent } from '@well-known-components/env-config-provider'
+import {
+  createServerComponent,
+  createStatusCheckComponent,
+  instrumentHttpServerWithPromClientRegistry
+} from '@well-known-components/http-server'
+import { createLogComponent } from '@well-known-components/logger'
+import { createMetricsComponent } from '@well-known-components/metrics'
+import { AppComponents, GlobalContext } from './types'
+import { metricDeclarations } from './metrics'
 
 // Initialize all the components of the app
 export async function initComponents(): Promise<AppComponents> {
-  const config = await createDotEnvConfigComponent({ path: [".env.default", ".env"] })
+  const config = await createDotEnvConfigComponent({ path: ['.env.default', '.env'] })
   const metrics = await createMetricsComponent(metricDeclarations, { config })
   const logs = await createLogComponent({ metrics })
   const server = await createServerComponent<GlobalContext>({ config, logs }, {})
   const statusChecks = await createStatusCheckComponent({ server, config })
-  const fetch = await createFetchComponent()
 
-  await instrumentHttpServerWithMetrics({ metrics, server, config })
+  await instrumentHttpServerWithPromClientRegistry({ metrics, server, config, registry: metrics.registry! })
 
   return {
     config,
     logs,
     server,
     statusChecks,
-    fetch,
-    metrics,
+    metrics
   }
 }
